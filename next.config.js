cc2992c2809797a83f059fd5170b0314f7dd8919
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-01-25 22:50:53 +0300

    AV

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: isProd ? "/nextjs/" : "",
+  images: {
+    unoptimized: true,
+  },
+};
